8f5cc4916072482872493a70385a2be40f9310e3
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 16:20:40 -0700

    the answer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+/**
+ * main - function to run the code
+ *
+ * printf: prints the target
+ *
+ * Description: print size on bytes
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("%d \n", sizeof(i));
+return (0);
+}
