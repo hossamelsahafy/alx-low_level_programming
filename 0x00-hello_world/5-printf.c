9a4c84e270573fc5894646d236a64b4b19eeac30
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 15:40:33 -0700

    the answer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Description: print some thing cool about art
+ *
+ * printf: to print the target
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
