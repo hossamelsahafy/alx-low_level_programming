f4237bdcbb6aef9b0984ae4beb20642db2ce787d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 15:14:26 -0700

    the answer

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this function for running the code
+ *
+ * puts:  prints the target
+ *
+ * Description: print something discripes programming
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
+}
