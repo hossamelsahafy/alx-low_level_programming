0ee1e2a7a72ad50814b5a1ab21a1613631fcd1b6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 17:35:49 -0700

    the answer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stddef.h>
 /**
  * main - function to run the code
@@ -16,10 +17,10 @@ long int li;
 long long int lli;
 float f;
 
-	printf("Size of a char:%zu \n", sizeof(s));
-	printf("Size of an int:%zu \n", sizeof(s));
-	printf("Size of a long int:%zu \n", sizeof(s));
-	printf("Size of a long long int:%zu \n", sizeof(s));
-	printf("Size of a float:%zu \n", sizeof(s));
+	printf("Size of a char:%zu \n", sizeof(c));
+	printf("Size of an int:%zu \n", sizeof(i));
+	printf("Size of a long int:%zu \n", sizeof(li));
+	printf("Size of a long long int:%zu \n", sizeof(lli));
+	printf("Size of a float:%zu \n", sizeof(f));
 return (0);
 }
