4e7e40e921a0221c21f06be10f462eefa242985f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 19:40:11 -0700

    the answer

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - run the code
+ *
+ * Description: print without printf command
+ *
+ * Return 1 Always
+ */
+int main(void)
+{
+	char buffer[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len=59;
+	write(2, buffer ,len);
+return (1);			
+}
