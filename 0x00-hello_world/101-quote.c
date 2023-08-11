fc7bc9a5e5ad097e86f1a2a4bb8a8dbf7c7a5934
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 19:48:38 -0700

    the answer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char buffer[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int len = 59;
 
 	write(2, buffer, len);
