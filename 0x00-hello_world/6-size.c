2cc26031f16f06d91aceaa1cd3360c64b35ac517
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-10 17:15:09 -0700

    the answer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,16 @@
  */
 int main(void)
 {
-	float i;
-	printf("%f \n", sizeof(i));
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char:%c \n", sizeof(s));
+	printf("Size of an int:%i \n", sizeof(s));
+	printf("Size of a long int:%ld \n", sizeof(s));
+	printf("Size of an long long int:%lld \n", sizeof(s));
+	printf("Size of an float:%f \n", sizeof(s));
 return (0);
 }
