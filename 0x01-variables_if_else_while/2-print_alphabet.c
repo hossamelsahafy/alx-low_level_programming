d7abcad10500c3e5bda7ac7ab8033924f71e3705
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-11 15:02:44 -0700

    the answer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Description: print some lower case alphabet
+ *
+ * putchar: to print the target
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char l;
+
+	for (l = a; l <= z; l++)
+	{
+		putchar(l);
+		putchar(\n)
+	}
+
+return (0);
+{
