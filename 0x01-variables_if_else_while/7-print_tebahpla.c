8207834a1ff0b87265b5f48166369876adcc02a2
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-11 19:38:18 -0700

    the asnwer

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Description: print letters
+ *
+ * putchar: to print the target
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a' c--)
+	{i
+		putchar (c);
+	}
+	putchar ('\n')i;
+return (0);
+}
