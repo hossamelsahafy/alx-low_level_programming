b041e2074b7f6c21fdac0c702016ad9542632c28
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-11 18:34:42 -0700

    the answer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Description: print some number
+ *
+ * putchar: to print the target
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar (i + '0');
+	}
+	putchar ('\n');
+return (0);
+}
