768b9b9b03f2307cc415f9ff86f493f12cc476b1
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-11 17:25:48 -0700

    the answer

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Description: print letters except some
+ *
+ * putchar: to print the target
+ *
+ *Return: 0 Always
+ */
+int main(void)
+{
+        char l;
+
+        for (l = 'a'; l <= 'z'; l++)
+        {
+                if (l != q && l != e)
+                {
+                        putchar (l);
+                }
+        }
+        putchar ('\n');
+return (0);
+}
