41cba56efe698293472bc342b02e5c76b5c92d23
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-11 17:32:02 -0700

    the answer

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@
  */
 int main(void)
 {
-        char l;
+	char l;
 
-        for (l = 'a'; l <= 'z'; l++)
-        {
-                if (l != 'q' && l != 'e')
-                {
-                        putchar (l);
-                }
-        }
-        putchar ('\n');
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' && l != 'e')
+		{
+			putchar (l);
+		}
+	}
+	putchar ('\n');
 return (0);
 }
