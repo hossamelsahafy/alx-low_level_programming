dcb13eb7a8023b6810d98d59f442231c61c533f9
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-12 08:20:03 -0700

    the naswer

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - this function to start coding
+ *
+ * Deiscription: print some numbers with commas  except
+ *
+ * putchar: to print the target
+ *
+ *Return: 0 Always
+ */
+
+int main(void)
+{
+	int i, j, g;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (g = j + i + 1; g < 10; g++)
+			{
+				putchar (i + '0');
+				putchar (j + '0');
+				putchar (g + '0');
+				if (i != 7 || j != 8 || g != 9)
+				{
+					putchar(',');
+					putchar(' ')
+				}
+			}
+		}
+	}
+	putchar ('\n');
+return (0);
+}
