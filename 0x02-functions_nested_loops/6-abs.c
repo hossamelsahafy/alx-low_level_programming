8f0918901f117bff1cc3acf66e6a537e5af9867d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-15 15:30:55 -0700

    the answer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * _putchar: print the target
+ *
+ * Description: printing absolute value of numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	int x = -5
+	int y = abs(x);
+	{
+		_putchar ('y');
+	}
+_putchar ('\n');
+return (0);
+}
