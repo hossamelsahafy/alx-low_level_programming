3be4f40483d4236c3553318ec64b32354fb2eb09
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-15 11:37:12 -0700

    the answer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "_putchar.c"
+#include "main.h"
+/**
+ * _islower - to check the lower character
+ * _putchar: print the target
+ * @c: character to check
+ * Description: checking for the lower chracter
+ * Return: 1 if c is lower case 0 otherwise
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a'; c <= 'z' c++)
+	{
+		_putchar(c);
+		return (1);
+	}
+	else
+	{
+		return (0);
+_putchar('\n');
+}
