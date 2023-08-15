3a302c734e2d410ad804f55eb87e34eec955935e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-15 15:07:00 -0700

    the answer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_sign - to check the positive ,negative and 0
+ *
+ * _putchar: print the target
+ *
+ * @n: character to check
+ *
+ * Description: checking for the positive and negative num
+ *
+ * Return: If n is greater than zero,
+ * the function returns 1 to indicate that n is positive.
+ *
+ * If n is equal to zero,
+ * the function returns 0 to indicate that n is zero.
+ *
+ * If n is less than zero,
+ * the function returns -1 to indicate that n is negative.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putcha('+');
+		return (-1);
+	}
+}
