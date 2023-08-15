28e400f3b4eac414bdb5f969a6dea9f049d55068
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-15 07:11:20 -0700

    the answer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - this function for running the code
+ *
+ * _putchar: prints the target
+ * Description: print alphabet
+ *
+ * Return: Always 0
+ */
 
 void print_alphabet(void)
 {
