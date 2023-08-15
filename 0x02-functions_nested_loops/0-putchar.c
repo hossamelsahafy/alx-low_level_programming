9067fae65f7947a8d679dafcb6d2c2a0618eba2e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-15 05:55:04 -0700

    the answer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-char u = '_';
+/**
+ * main - this function for running the code
+ *
+ * _putchar: prints the target
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
+	int i;
+	char u = 95;
+
+	for (i = 0; i < 1; i++)
+	{
 		_putchar(u);
 		_putchar('p');
 		_putchar('u');
@@ -12,5 +25,6 @@ int main(void)
 		_putchar('a');
 		_putchar('r');
 		_putchar('\n');
+	}
 return (0);
 }
