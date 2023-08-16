f004d4a4fcce5d44d941bf7b699c4d623792b43e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-16 14:56:29 -0700

    the answer

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - function to print time table
+ *
+ * Description: print time table
+ *
+ * @n:to identify number
+ *
+ * _putchar: to print the target
+ */
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n x++)
+		{
+			_putchar(0);
+			for (y = 1; y <= n y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				z = x * y;
+				if (z <= 9)
+					_putchar(' ');
+				if (z <= 99)
+					_putchar(' ');
+				if (z >= 100)
+				{
+					_putchar(z / 100 + '0');
+					_putchar(z / 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+				else if (z <= 100 && z >= 10)
+					_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			_putchar('\n');
+		}
+
+	}
+}
+
+
+
+
+
