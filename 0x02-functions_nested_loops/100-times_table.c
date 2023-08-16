e8e4378b29ab9d7f787f259138de05ada20bd385
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-16 15:49:00 -0700

    the answer

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,8 +32,7 @@ void print_times_table(int n)
 				if (z >= 100)
 				{
 					_putchar((z / 100) + 48);
-					_putchar((z / 10) + 48);
-					_putchar((z % 10) + 48);
+					_putchar((z / 10) % 10 + 48);
 				}
 				else if (z <= 100 && z >= 10)
 					_putchar((z / 10) + 48);
