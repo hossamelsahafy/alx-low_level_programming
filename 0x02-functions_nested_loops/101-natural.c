0f1e3827e0e6581e531b36bef6f3cfeed4ee3246
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-16 16:09:55 -0700

    the answer

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the sum of 3 or 5 below 1024
+ *
+ * printf: to print the target
+ *
+ * Return:  always 0
+ */
 
 int main(void)
 {
+	int i;
 	int sum = 0;
+
 	for (int i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
