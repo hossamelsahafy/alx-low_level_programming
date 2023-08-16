8f3289847b90b85d9ac9876d59736e6406c519e7
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-16 07:05:03 -0700

    the answer

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9 i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (j = 0; j < 9; j++)
 		{
@@ -22,6 +22,7 @@ void times_table(void)
 			_putchar(',');
 			_putchar(j % 10 + '0');
 			_putchar(j / 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
