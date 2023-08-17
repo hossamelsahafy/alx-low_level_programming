fc89414f23d0214a633dbf75bc2faaa2502cbd47
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-17 10:34:18 -0700

    the  answer

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+        printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
