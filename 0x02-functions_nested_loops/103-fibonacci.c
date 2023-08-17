cb65057b956b9e621b398116524dea5cbc309e20
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-16 17:10:14 -0700

    the answer

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry function
+ *
+ * Description: print even numbers doesnt exceed 4 million
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	float t;
+	unsigned long i = 0, j = 1, z;
+
+	while (1)
+	{
+		z = i + j;
+
+		if (z > 4000000)
+		{
+			break;
+		}
+		if ((z % 2) == 0)
+		{
+			t += z
+		}
+
+		i = j;
+		j = z;
+	}
+	printf("%.0f\n", t);
+
+	return (0);
+}
