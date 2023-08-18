5b6b84cce166dd6d6bec76ff3d904820c93f8728
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-18 14:56:46 -0700

    the answer

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - function to print diagonal
+ * @n: parameter t identify number
+ * Return: always n
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar ('/');
+		putchar ('\n');
+	}
+
+}
