f9e9ed0a446e56d80343cd55303e193adef15846
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-18 09:29:48 -0700

    the answer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - function to multiply 2 numbers
+ *
+ * @a: parameter to identify numbers
+ *
+ * @b: prameter to identify numbers
+ *
+ * Return: x always
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+return (x);
+}
