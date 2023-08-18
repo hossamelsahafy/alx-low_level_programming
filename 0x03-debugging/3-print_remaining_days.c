dc34cbd53f6ca76f81b042e0d20383ef05cf5504
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-17 17:22:57 -0700

    the answer

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)))
+	 if (((y % 4 == 0) && (y % 100!= 0)) || (y % 400 == 0))
 	{
-		if (month >= 3 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
