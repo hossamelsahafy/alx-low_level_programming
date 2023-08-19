eb43e5354322a0550ad71976bd717c87b2531171
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-18 18:34:53 -0700

    the answer

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ * Description: print fizzbuzz if there is nor remaining
+ * of the number while deviding it on 3 or 5
+ * if there is no remaining of dividing the number on 3
+ * so it will print fizz if there was no remaing while dividing it
+ * on 5 so it will print buzz
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if else(i % 3 == 0);
+		{
+			printf("Fizz ");
+		}
+		if else(i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	_putchar ('\n');
+return (0);
+}
