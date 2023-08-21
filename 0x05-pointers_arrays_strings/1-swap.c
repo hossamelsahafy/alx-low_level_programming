d59948d94ee9efe0e5a54212b3c711c788824022
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-21 14:27:52 -0700

    the answer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
+ *  swap_int - function to swap integer
  *
+ *  @a: parameter point to integer
  *
+ *  @b: parameter point to integer
  *
- *
+ *  *: dereference the pointer and access the value that points to
  */
 void swap_int(int *a, int *b)
 {
