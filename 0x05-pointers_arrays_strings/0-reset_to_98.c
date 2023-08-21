b018fa413db595d2c424b077f12d1e2624b70a60
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-21 14:07:53 -0700

    the answer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to print value of n
+ *
+ * @n: parameter is pointer to integer
+ *
+ * *: dereference the pointer and access the value that points to
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define Main_h
+#include <stdio.h>
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
