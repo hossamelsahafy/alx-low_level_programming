b263aab037a5de9c4f8327111cc7a5bf6bf755d1
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-21 18:10:22 -0700

    the answer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - function to print string
+ *
+ * @s: parameter point to char
+ */
+void rev_string(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	if (len > 1)
+	{
+		char tmp;
+
+		tmp = s[0];
+		s[0] = s[len - 1];
+		s[len - 1] = '\0';
+		rev_string(s + 1);
+		s{len - 1} = tmp;
+	}
+}
