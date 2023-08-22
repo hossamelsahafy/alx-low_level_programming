b952f0e3161bd13e7ad7388081b15a5bdf833a76
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-22 02:46:49 -0700

    the answer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,2 +1,40 @@
 #include "main.h"
+/**
+ * _atoi - function to convert string into integer
+ *
+ * @s: parameter point to char array
+ *
+ * Return: result of multiplying sign & result
+ */
 
+int _atoi(char *s)
+{
+	int sign;
+	int result;
+	int started;
+
+	sign = 1;
+	result = 0;
+	started = 0;
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign = -sign;
+			started = 1;
+		}
+		else if (*s >= '0' && *s <= 9)
+		{
+			result = result * 10 + (*s - '0');
+			started = 1;
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
+	return (sign * result);
+
+
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = rand() % ('~' - '!') + '!';
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return 0;
+}
