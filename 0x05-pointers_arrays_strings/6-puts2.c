19c40778fb96bce09ba895f159cc52c82b4e5105
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-21 18:39:39 -0700

    the answer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character of a string
+ *
+ * @str: parameter that point to char
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i += 20)
+	{
+		putchar (str[i]);
+	}
+	putchar ('\n');
+}
