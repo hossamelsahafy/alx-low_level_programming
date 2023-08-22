9d5fa38ffbad1faf0af2e58c1c2b05419a8be709
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-22 04:18:55 -0700

    the answer

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - entry point 
+ *
+ * Return: always 0
+ */
+
 #define PASSWORD_LENGTH 12
 
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
+	char c;
 	int i;
 
 	srand(time(NULL));
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	for (i = 0; i < 2772; i += c)
 	{
-		password[i] = rand() % ('~' - '!') + '!';
+		c = rand() % 128;
+		if ((i + c) > 2772)
+			break;
+		putchar ('c');
 	}
-	password[PASSWORD_LENGTH] = '\0';
-	printf("%s\n", password);
-	return 0;
+	printf("%c", 2772 - i)
+	return (0);
+
 }
