4e2fd4b65d78667c59583f9de58305712c0704e4
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-21 19:47:27 -0700

    the answer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - function to copy string
+ *
+ * @dest: parameter point to the buffer
+ *
+ * @src: parameter point to the string that will be copied
+ *
+ * Return: itt will return to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p;
+
+	*p = dest;
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+	*p = '\0';
+
+	return (dest);
+}
