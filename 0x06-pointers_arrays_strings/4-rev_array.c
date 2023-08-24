e549e2e8ff3b2a7e7d4305d7ddcdd74939983fc4
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 12:28:23 -0700

    the answer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - function to reverse array
+ *
+ * @a: parameter point to integer
+ *
+ * @n: parameter to identfy integer
+ */
+void reverse_array(int *a, int n)
+{
+	int r[1500];
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		r = a[n - i -1];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = r[i];
+	}
+}
