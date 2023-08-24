1476fcd4deee0a54c01327510754c534d8e887a3
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 09:18:16 -0700

    the answer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,15 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	while (i < n)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
+	for (i = 0; i < n && scr[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
 	while (i < n)
 	{
 		dest[i] = '\0';
