bdf66df07003d7f380276895f30d5c5c0847c003
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-23 18:22:00 -0700

    the answer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - function to concatenates two strings
+ *
+ * @dest: parameter point to char
+ *
+ * @src: parameter point to char
+ *
+ * @n: parameter point to integer
+ *
+ * Return: will be dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	int count = 0;
+
+	while (*p != '\0');
+	{
+		p++;
+	}
+	while (*src != '\0' && p - dest < sizeof(dest) - 1 && count < n)
+	{
+		*p = *src;
+		src++;
+		count++;
+		p++;
+	}
+	*p = '\0';
+	return (dest);
+}
