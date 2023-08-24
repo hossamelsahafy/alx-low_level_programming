2cc95b84ed48ec7a53454b47356fd0d666fe6e0a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 16:37:40 -0700

    the answer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * cap_string - function to capitalize all words of string
+ *
+ * @str: parameter to point to char
+ *
+ * Return: will be str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '\t')
+		{
+			str[i] = '.';
+		}
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+	return (str);
+}
