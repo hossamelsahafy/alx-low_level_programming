b9d38c16f98043966d8274f9647dc6b9a0ecaa7d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-23 17:21:07 -0700

    the answer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,13 +18,13 @@ char *_strcat(char *dest, char *src)
 	while (dest[i] != '\0')
 	{
 		i++;
-		j = 0;
-		while (src[i] != '\0')
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
+	}
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
