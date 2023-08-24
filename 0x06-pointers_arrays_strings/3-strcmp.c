29f7b67b385986419ffad3478916d3c1914442b7
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 11:53:56 -0700

    the answer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,32 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i; 
-	int result;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0'; || s2[i]; i++)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == s2[i])
 		{
-			return (s1[i] - s2[i]);
+			i++;
+			continue;
 		}
+		if (s1[i] >= 'A' && s1[i] <= 'Z')
+		{
+			if (s1[i] + 32 == s2[i])
+			{
+				i++;
+				continue;
+			}
+		}
+		if (s2[i] >= 'A' && s2[i] <= 'Z') 
+		{
+			if (s2[i] + 32 == s1[i])
+			{
+				i++;
+				continue;
+			}
+		}
+		return s1[i] - s2[i];
 	}
-	return (0);
-
+	return s1[i] - s2[i];
 }
