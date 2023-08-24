97c8d9e671dc8a2e5b5a1956d71e92661d9a43fd
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 15:01:59 -0700

    the answer

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - fuction to change lowcase letter to upper
+ *
+ * @str: parameter that point to char
+ *
+ * Return: will be str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
