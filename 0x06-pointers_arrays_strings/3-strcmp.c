db124bc480fac79ed87dab63fec9825f14afe744
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-24 11:26:01 -0700

    the answer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,14 +13,13 @@ int _strcmp(char *s1, char *s2)
 	int i; 
 	int result;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i]; i++)
 	{
-		result  = s1[i] - s2[i];
-		if (result != '\0')
+		if (s1[i] != s2[i])
 		{
-			return (result);
+			return (s1[i] - s2[i]);
 		}
 	}
-	return (s1[i] - S2[i]);
+	return (0);
 
 }
