66157175ca6544dde740bb31c2085532ab358bee
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-25 08:18:02 -0700

    the answer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - function to replace letter
+ *
+ * @str: paramater point to char
+ *
+ * Return: will be str
+ */
+
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' i++)
+	{
+		if (str[i] >= 'a' str[i] < 'n')
+		{
+			str[i] = str[i] + 13;
+		}
+		else if (str[i] >= 'n' str[i] <= 'z')
+		{
+			str[i] = str[i] - 13;
+		}
+	}
+	return (str);
+}
