407d620847273e2625bf4f38bca41af2425819d0
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-25 15:19:01 -0700

    the answer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,21 +12,20 @@ char *cap_string(char *str)
 {
 	int i;
 
-	i = 0;
 	while (str[i] != '\0')
 	{
-		if (i == 0 || str[i] == ' ' || str[i] == '\t'
-				|| str[i] == '\n' || str[i] == ',' || str[i] == ';'
-				|| str[i] == '.'  || str[i] == '?' || str[i] == '"'
-				|| str[i] == '!'  || str[i] == '(' || str[i] == ')'
-				|| str[i] == '{'  || str[i] == '}')
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t'
+				|| str[i - 1] == '\n'|| str[i - 1] == ',' || str[i - 1] == ';'
+				|| str[i - 1] == '.' || str[i - 1] == '?' || str[i - 1] == '"'
+				|| str[i - 1] == '!' || str[i - 1] == '(' || str[i - 1] == ')'
+				|| str[i - 1] == '{' || str[i - 1] == '}')
 		{
-			if (str[i - 1] >= 'a' && str[i - 1] <= 'z')
-				str[i - 1] = str[i - 1] - 32;
-			if (str[i] == '\t')
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+			/*if (str[i] == '\t')
 			{
 				str[i] = ' ';
-			}
+			}*/
 		}
 		i++;
 	}
