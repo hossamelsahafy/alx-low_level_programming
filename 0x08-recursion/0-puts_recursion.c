86197e7da36b08cf40fe61e8703c07f2e527860b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 14:22:13 -0700

    the answer

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print string followed by new line
+ *
+ * @s: parameter  poin to char
+ *
+ * Return: to exit the function incase it has printed all characters
+ */
+void _puts_recursion(char*s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recurtion(s + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define Main_h
+#include <stdio.h>
+#include <string.h>
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
