bd76eedc8b4bb84bbdaee8df98f299acb9d3f3e6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 18:23:10 -0700

    the answer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,33 +6,23 @@
  *
  * @n: parameter to identify integer
  *
- * Return: if i * i == n it will be 1 else it will be -1
+ * Return: if i * i == n it will be result else it will be -1
  */
 int _sqrt_recursion(int n)
 {
 	static int i = 1;
 
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n <= 1)
-	{
-		return (n);
-	}
-	if (i * i >= n)
-	{
-		int result = i;
-
-		i = 1;
-		return (result);
-	}
 	if (i * i == n)
 	{
 		int result = i;
-
-		i = 1;
+	
+		i = 0;
 		return (result);
+	}
+	if (i * i > n)
+	{
+		i = 0;
+		return (-1);
 	}
 	i++;
 	return (_sqrt_recursion(n));
