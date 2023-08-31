e48464659850f256768eacb1520a698fc27e1957
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 18:32:49 -0700

    the answer

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int helper(int n, int i)
+int helper(int n, int i);
 #endif
