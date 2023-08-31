68f835321f9257e1468b9085c5aaebb59aaa091b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 18:31:22 -0700

    the answer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,24 +6,28 @@
  *
  * @n: parameter to identify integer
  *
+ * @i: to identfy integer
+ *
  * Return: if i * i == n it will be result else it will be -1
+ */
+int helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (helper(n, i + 1));
+}
+/**
+ * _sqrt_recursion - function to return the natural square root of a number
+ *
+ * @n: parameter to identify integer
+ *
+ * Return: will be the natural square root of n if it exists, otherwise -1
  */
 int _sqrt_recursion(int n)
 {
-	static int i = 1;
-
-	if (i * i == n)
-	{
-		int result = i;
-	
-		i = 0;
-		return (result);
-	}
-	if (i * i > n)
-	{
-		i = 0;
+	if (n < 0)
 		return (-1);
-	}
-	i++;
-	return (_sqrt_recursion(n));
+	return (helper(n, 0));
 }
