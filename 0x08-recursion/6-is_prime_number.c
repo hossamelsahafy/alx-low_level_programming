4fdfa9199a3c4791519ec2aa083892801650ecfc
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 19:19:20 -0700

    the answer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,12 +22,12 @@ int is_divisible(int n, int i)
 
 		return (1);
 	}
-	return is_divisible(n, i + 1);
+	return (is_divisible(n, i + 1));
 }
 
 /**
- * is_prime_number - function to return 1 if the input integer is a prime number
- * otherwise return 0
+ * is_prime_number - function to return 1 if the input 
+ * integer is a prime number, otherwise return 0
  *
  * @n: parameter ti identify integer
  *
