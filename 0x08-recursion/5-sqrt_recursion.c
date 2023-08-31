d50ff22a0af0616d949c7b29ef515771ca54121a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-30 18:36:23 -0700

    the answer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - function to return the natural square root of a number
+ * MEOW_FOR_HELP - function to return the natural square root of a number
  *
  * @n: parameter to identify integer
  *
@@ -10,13 +10,13 @@
  *
  * Return: if i * i == n it will be result else it will be -1
  */
-int helper(int n, int i)
+int MEOW_FOR_HELP(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	if (i * i > n)
 		return (-1);
-	return (helper(n, i + 1));
+	return (MEOW_FOR_HELP(n, i + 1));
 }
 /**
  * _sqrt_recursion - function to return the natural square root of a number
@@ -29,5 +29,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (helper(n, 0));
+	return (MEOW_FOR_HELP(n, 0));
 }
