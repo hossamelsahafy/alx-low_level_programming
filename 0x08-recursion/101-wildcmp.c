8a172fb3e5e4fb59230084120cf571f335b6694f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-08-31 08:10:58 -0700

    the answer

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * wildcmp - function to  compare two strings and returns 1
+ * if the strings can be considered identical, otherwise return 0.
+ *
+ * @s1: parameter that point to char
+ *
+ * @s2: parameter that point to char
+ *
+ * Return: if (*s1 == '\0' && *s2 == '\0') return will be 1
+ * if (*s2 == '*') return will be return (wildcmp(s1, s2 + 1) ||
+ * (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+ * if  if (*s1 == *s2) return will be return ( wildcmp(s1 + 1, s2 + 1))
+ * otherwise it will be 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0'
+					&& wildcmp(s1 + 1, s2)));
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,4 +13,5 @@ int is_divisible(int n, int i);
 int is_prime_number(int n);
 int is_palindrome_rec(char *s, int i, int j);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
