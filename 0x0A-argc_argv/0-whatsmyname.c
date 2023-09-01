3fbcdfa891492272bab6ac88bfcaf8b9f8338286
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-01 15:09:44 -0700

    the answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - function to print its name, followed by a new line
+ *
+ * @argc: parameter to identfy integer
+ *
+ * @argv[]: parameter to identfy string
+ *
+ * Return: will be 0
+ */
+int main(int argc, char argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
