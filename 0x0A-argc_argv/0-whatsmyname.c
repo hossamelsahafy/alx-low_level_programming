f44f83a4030024aa877fe897dfebbc93f5b4f934
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-01 15:37:29 -0700

    the answer

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,12 +6,8 @@
  * @argc: parameter to identfy integer
  *
  * @argv: parameter to identfy string
- *
- * Return: will be 0
  */
 int main(int argc, char *argv[])
 {
-
 	printf("%s\n", argv[0]);
-	return (0);
 }
