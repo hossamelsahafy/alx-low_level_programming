9ca61458f73a0e702d2c56da90c6a8d8bdaa8709
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-01 17:55:23 -0700

    the answer

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * main - function to print the result of the
+ * multiplication, followed by a new line
  *
+ * @argc: parameter to identify integer
  *
+ * @argv: parameter point to char
  *
- *
+ * Return: if argc != 3 it will be 1 otherwise 0
  */
 int main(int argc, char *argv[])
 {
+	int result;
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = a * b;
+		printf("%d\n", result);
+	}
+	return (0);
+}
