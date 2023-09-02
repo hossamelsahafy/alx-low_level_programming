61ea3bcf315e77cbdf7ea4a6b613861218d8d072
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-01 19:27:46 -0700

    the answer

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,21 +21,22 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[i]);
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	coins = coins + cents / 25;
+	coins = 0;
+	coins = coins + (cents / 25);
 	cents = cents % 25;
-	coins = coins + cents / 10;
+	coins = coins + (cents / 10);
 	coins = coins % 10;
-	coins = coins + cents / 5;
+	coins = coins + (cents / 5);
 	cents = cents % 5;
-	coins = coins + cents / 2;
+	coins = coins + (cents / 2);
 	cents = cents % 2;
-	coins = coins + centes;
+	coins = coins + cents;
 	printf("%d\n", coins);
 	return (0);
 }
