4adb11a4634a4299406caf652f42afae5f2d791f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-04 16:26:55 -0700

    the answer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,8 +9,8 @@
  *
  * @s2: parameter that point to char
  *
- * Return: if s1 & s2 == NULL return will be ""
- * otherwise p
+ * Return: if p == NULL it will return NULL
+ * otherwise it will return p
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -18,21 +18,18 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0;
 	int len2 = 0;
 
-	if (p == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 "";
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	p = malloc(len1 + len2 + 1);
-	if (s1 == NULL)
-	{
-		return ("");
-	}
-	if (s2 == NULL)
-	{
-		return ("");
-	}
+
 	if (p == NULL)
 	{
 		return (NULL);
