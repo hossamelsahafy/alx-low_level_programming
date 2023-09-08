dc3a947eabf2f786658edabadbb8c85ab80fc907
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-08 10:16:36 -0700

    the answer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		return (NULL);
 	if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	n_p = malloc(new_size);
