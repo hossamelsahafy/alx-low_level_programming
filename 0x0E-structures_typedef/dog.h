31b14a1c7a467a0fa0144b4694806b1cb646beef
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-11 14:58:57 -0700

    the answer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 
 #define DOG_H
-
+/**
+ * struct dog - type to discripe dog
+ *
+ * @name: parameter that point to the name of the dog
+ *
+ * @age: parameter to indentify the age of the dog
+ *
+ * @owner: parameter to point to the owner of the dog
+ */
 struct dog
 {
 	char *name;
-       	float age;
+	float age;
 	char *owner;
 };
+
 #endif
