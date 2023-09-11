4f64ed2b8eb9bf33ce44732a90423fe0c3ea7013
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-11 16:56:01 -0700

    the answer

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function to free dogs
+ *
+ * @d: parameter point to dog_t
+ *
+ * Return: will return to the function without freeing anything
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free (d->name);
+	free (d->owner);
+	free (d);
+}
