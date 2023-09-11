f3bac8e40ac066a9cf5a4c53b5ecd11a66b7f03e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-11 16:02:40 -0700

    the answer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,15 +10,15 @@
  *
  * @owner: parameter to point to the owner of the dog
  */
-typedef struct dog
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-void init_dog(struct dog_t *d, char *name, float age, char *owner);
+};
+typedef struct dog dog_t
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
-void print_dog(struct dog_t *d);
+void print_dog(struct dog *d);
 
 #endif
