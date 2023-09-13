8f76f89e1eb52a65b7e5f36e62e268aa904db787
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-13 16:35:08 -0700

    the answer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,13 +10,11 @@
  * @size: parameter to identify size_t
  *
  * @action: parameter point to function
- *
- * Return: will be zero
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
