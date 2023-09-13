eaaeebc6957dc62f0b6cbdb0238228434c6f0215
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-13 13:17:36 -0700

    the answer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,11 +8,9 @@
  *
  * @f: parameter point to f
  *
- * @c: parameter point to char
- *
  * Return: if  name or f  == NULL i will exit the function
  */
-void print_name(char *name, void (*f)(char *c))
+void print_name(char *name, void (*f)(char *))
 {
 
 	if (name == NULL && f == NULL)
