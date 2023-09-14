597140e0446f35c9be5a4b4877db8f194dcb050d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-14 11:18:41 -0700

    the answer

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
  *
  * @n: parameter to idntify number of argyments
  *
- * Return: sum of arguments
+ * Return:if n == 0 it will be 0 otherwise sum of arguments
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -15,11 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	int sum = 0;
 
+	if (n == 0)
+		return (0);
 	va_start(args, n);
 	for (count = 0; count < n; count++)
 	{
-		if (n == 0)
-			return (0);
 		sum = sum + va_arg(args, int);
 	}
 	va_end(args);
