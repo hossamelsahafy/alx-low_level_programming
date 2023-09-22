994dfef927236f509a651be5a5ba48219c97ca4a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-22 08:58:22 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@ void __attribute__((constructor)) b_main();
  * b_main -  function to  print before main function
  */
 
-void b_main()
+void b_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
