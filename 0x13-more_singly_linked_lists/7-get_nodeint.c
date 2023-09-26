d26246292a9bdabee197789566e7dcdf1984b26a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-25 18:03:44 -0700

    update to header & get_nodeint.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,9 +14,9 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int n;
+	unsigned int i;
 
-	for (i = 0; i < index; && head != NULL; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
 	}
