394f3fb815b49752b145cd127bcd0c7f8a379d2c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-26 14:23:36 -0700

    update file to header & insert_nodeint

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - function to
+ * insert a new node at a given position
+ *
+ * @head: parameter point to listint_t
+ *
+ * @idx: parameter to identify unsigned int
+ *
+ * @n: parameter to identify integer
+ *
+ * Returns: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *n_node;
+	listint_t *tmp;
+	unsigned int i = 0;
+
+	n_node = malloc(sizeof(listint_t));
+	if (n_node == NULL)
+	{
+		return (NULL);
+	}
+	n_node->n = n;
+	if (idx == 0)
+	{
+		n_node->next = *head;
+		*head = n_node;
+		return (n_node);
+	}
+	tmp = *head;
+	while (tmp != NULL && i < idx - 1)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL || i > idx - 1)
+	{
+		free(n_node);
+		return (NULL);
+	}
+	n_node->next = tmp->next;
+	tmp->next = n_node;
+	return(n_node);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef lists_H
 #define lists_H
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -32,4 +33,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 int sum_listint(listint_t *head);
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif
