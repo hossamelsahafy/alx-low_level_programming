a9ccc549db792d256b26f35fc4bc4f4cf4913e88
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-09-28 15:46:41 -0700

    update to set_bit file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,10 +14,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8 - 1)
+	if (index > sizeof(unsigned long int) * 8 - 1)
 	{
 		return (-1);
 	}
-	*n = *n || (1UL << index);
-	rerturn (1);
+	*n = *n | (1 << index);
+	return (1);
 }
