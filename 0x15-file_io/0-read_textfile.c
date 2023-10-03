bcf3f7df2ba3ef69825e0d9713f995d663f672ff
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-03 23:37:40 +0300

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - function to read a text file and prints
@@ -18,32 +20,42 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t n_read = 0;
-	ssize_t n_write = 0;
-	char *buff;
-	FILE *fp;
+	int fd;
+    char *buff;
+    ssize_t b_read, b_write;
 
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL || filename == NULL)
-	{
-		return (0);
-	}
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-	{
-		return (0);
-	}
-	n_read = fread(buff, sizeof(char), letters, fp);
-	fclose(fp);
-	if (n_read == 0)
-	{
-		return (0);
-	}
-	n_write = fwrite(buff, sizeof(char), n_read, stdout);
-	free(buff);
-	if (n_write == 0 || n_write != n_read)
-	{
-		return (0);
-	}
-	return (n_read);
+    if (filename == NULL)
+    {
+        return (0);
+    }
+    if (filename == NULL)
+    {
+        return (0);
+    }
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        return (0);
+    }
+    buff = malloc(sizeof(char) * letters);
+    if (buff == NULL)
+    {
+        return (0);
+    }
+    b_read = read(fd, buff, letters);
+    if (b_read == -1)
+    {
+        return (0);
+    }
+    close(fd);
+    b_write = write(STDOUT_FILENO, buff, b_read);
+    if (b_write == -1 || b_write != b_read)
+    {
+        return (0);
+    }
+    free(buff);
+    return (b_write);
 }
+
+
+	
