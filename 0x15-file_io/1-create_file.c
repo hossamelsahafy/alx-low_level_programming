be699dc1b76823a900540dab93cea94698a3dc8f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-04 01:27:28 +0300

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+/**
+ * create_file - function to create a file
+ *
+ * @filename: parameter that point to const char
+ *
+ * @text_content: parameter that point to char
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”)
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+*/
 int create_file(const char *filename, char *text_content)
 {
-    FILE *fp;
-    int n;
+	int fp;
+	ssize_t l = 0;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-    fp = fopen(filename, "w");
-    if (fp == NULL)
-    {
-        return (-1);
-    }
-    chmod(filename, 0600);
-    if (text_content == NULL)
-    {
-        fclose(fp);
-        return (1);
-    }
-    n = fprintf(fp, "%s", text_content);
-    if (n < 0)
-    {
-        fclose(fp);
-        return (-1);
-    }
-    fclose(fp);
-    return (1);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while  (text_content[l] != '\0')
+		{
+			l++;
+		}
+		if (write(fp, text_content, l) == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+		else
+		{
+			if (write(fp, "", 0) == -1)
+			{
+				close(fp);
+				return (-1);
+			}
+		}
+	}
+	close(fp);
+	return (1);
 }
