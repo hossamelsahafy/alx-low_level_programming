08d1ba2aabe2f20089ded033b338e24c17b9f22a
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-03 23:43:55 +0300

    update to read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,41 +21,39 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-    char *buff;
-    ssize_t b_read, b_write;
+	char *buff;
+	ssize_t b_read, b_write;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
-    buff = malloc(sizeof(char) * letters);
-    if (buff == NULL)
-    {
-        return (0);
-    }
-    b_read = read(fd, buff, letters);
-    if (b_read == -1)
-    {
-        return (0);
-    }
-    close(fd);
-    b_write = write(STDOUT_FILENO, buff, b_read);
-    if (b_write == -1 || b_write != b_read)
-    {
-        return (0);
-    }
-    free(buff);
-    return (b_write);
-}
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
 
-
-	
+	if (buff == NULL)
+	{
+		return (0);
+	}
+	b_read = read(fd, buff, letters);
+	if (b_read == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	b_write = write(STDOUT_FILENO, buff, b_read);
+	if (b_write == -1 || b_write != b_read)
+	{
+		return (0);
+	}
+	free(buff);
+	return (b_write);
+}	
