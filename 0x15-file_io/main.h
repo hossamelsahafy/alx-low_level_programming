967ffe3a98ca28eab6e2afb39bd25cff532b03a2
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-03 22:11:39 +0300

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,8 @@
-#ifndef MIAN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-ssize_t read_textfile(const char *filename, size_t letters);
+#include <stddef.h>
 
-int create_file(const char *filename, char *text_content);
+size_t read_textfile(const char *filename, size_t letters);
 
 #endif
