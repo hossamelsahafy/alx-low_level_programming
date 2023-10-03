f45a2e6ba03da6c8fcbfe7613c4a531b04b7194b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-10-03 22:12:31 +0300

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,49 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters)
+/**
+ * read_textfile - function to read a text file and prints
+ * it to the POSIX standard output
+ *
+ * @filename: parameter point to const char
+ *
+ * @letters: parameter to identify size_t
+ *
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+
+size_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *fp;
+	size_t n_read = 0;
+	size_t n_write = 0;
+	char *buff;
+	FILE *fp;
 
-    fp = fopen("filename", "w");
-
-
-    fclose(fp);
-
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL || filename == NULL)
+	{
+		return (0);
+	}
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (0);
+	}
+	n_read = fread(buff, sizeof(char), letters, fp);
+	fclose(fp);
+	if (n_read == 0)
+	{
+		return (0);
+	}
+	n_write = fwrite(buff, sizeof(char), n_read, stdout);
+	free(buff);
+	if (n_write != n_read)
+	{
+		return (0);
+	}
+	return (n_read);
 }
