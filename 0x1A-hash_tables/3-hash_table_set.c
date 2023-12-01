fe25e47f3ed97b2a34a7519a1149de304c22d0c8
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-01 18:53:39 +0200

    function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,50 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ *
+ * @ht: parameter that point to hash_table_t
+ *
+ * @key: parameter that point to const char
+ *
+ * @value: parameter that point to const char
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
     hash_node_t *n_n, *tmp;
 
-    if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-    {
+    if (!ht || !key || !*key || !value || !(n_n = malloc(sizeof(hash_node_t))) || !(n_n->key = strdup(key)) || !(n_n->value = strdup(value)))
         return (0);
-    }
+    
     index = key_index((const unsigned char *)key, ht->size);
     tmp = ht->array[index];
-    while (tmp != NULL)
+    
+    while (tmp)
     {
-        if (strcmp(tmp->key, key) == 0)
+        if (!strcmp(tmp->key, key))
         {
             free(tmp->value);
-            tmp->value = strdup(value);
-            if (tmp->value == NULL)
-            {
-                return (0);
-            }
+            tmp->value = n_n->value;
+            free(n_n->key);
+            free(n_n);
             return (1);
         }
         tmp = tmp->next;
     }
-    n_n = malloc(sizeof(hash_node_t));
-    if (n_n == NULL)
-    {
-        return (0);
-    }
-    n_n->key = strdup(key);
-    if (n_n->key == NULL)
-    {
-        free(n_n);
-        return (0);
-    }
-    n_n->value = strdup(value);
-    if (n_n->value == NULL)
-    {
-        free(n_n->key);
-        free(n_n);
-        return (0);
-    }
+    
     n_n->next = ht->array[index];
     ht->array[index] = n_n;
     return (1);
 }
+
