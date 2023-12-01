c6090641a70fccfa0dc148ce662c21b6d28b3a73
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-01 18:16:58 +0200

    function to return the index at which the key/value pair should be stored in the array of the hash table

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,9 @@
  *
  * @size: parameter that point to unsigned long int
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair
+ * should be stored in the array of the hash table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
