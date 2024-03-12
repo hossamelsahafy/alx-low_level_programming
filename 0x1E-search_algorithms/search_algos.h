e01346792f4bf25bdd0b2ab07aaf5c8b65ff63bb
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-12 18:08:59 +0200

    File

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,6 @@ int linear_search(int *array, size_t size, int value);
 
 int binary_search(int *array, size_t size, int value);
 
-void Print_Array_Helper(int *array, size_t start, size_t end)
+void Print_Array_Helper(int *array, size_t start, size_t end);
 
 #endif
